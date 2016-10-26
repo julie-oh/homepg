7f4f30dfbc215147fdac6c17055ff439bc7004e8
oh <yj_oh@naver.com>
2016-10-26 21:21:41 +0900

    go

--- html/test/login_ok.php
+++ html/test/login_ok.php
@@ -8,10 +8,10 @@
     
     if(!isset($members[$user_id])){
         echo "<script>alert('아이디 또는 패스워드가 잘못되었습니다');histoty.back();</script>";
-    }
-    
-    if($members[$user_id]['pw']!=$user_pw){
+    }else if($members[$user_id]['pw']!=$user_pw){
         echo "<script>alert('아이디 또는 패스워드가 잘못되었습니다');history.back();</script>";
+    }else{
+        echo "<script>alert('성공함요');";
     }
     
     session_start();
