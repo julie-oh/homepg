54aa562a806f49d1469cdfc4f7e2499df7d5ada3
oh <yj_oh@naver.com>
2016-11-02 21:27:05 +0900

    정리

--- html/main_page.php
+++ html/main_page.php
@@ -184,20 +184,16 @@ $(function(){
                           <li class="padLR20"><a href="notice.html">공지사항</a></li>
                           <li class="padLR20"><a href="#">스케줄 관리</a></li>
                           <li class=" dropdown padLR20"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">회의실<span class="caret"></span></a>
-                              <ul class="dropdown-menu">
-                                  <li><a href="metting.html">팀회의</a></li>
-                                  <li><a href="#">임원직 회의</a></li>
-                              </ul>
-                          </li>
+                          <li class="padLR20"><a href="metting.html">팀회의</a></li>
                           <li class=" dropdown padLR20"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">결재문서 <span class="caret"></span></a>
                               <ul class="dropdown-menu">
                                   <li><a href="document_form.html">품의/제안서</a></li>
-                                  <li><a href="#">결재완료건</a></li>
+                                  <li><a href="#">결재진행/완료건</a></li>
                               </ul>
                           </li>
-                          <li class=" down padLR20"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">익명게시판<span class="caret"></span></a>
+                          <li class=" down padLR20"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">게시판<span class="caret"></span></a>
                               <ul class="dropdown-menu">
-                                  <li><a href="#">건의사항</a></li>
+                                  <li><a href="aboard/index.php">건의사항</a></li>
                                   <li><a href="board/index.php">자유게시판</a></li>
                               </ul>
                           </li>
