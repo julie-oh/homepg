eed88a1b21cfa842e724fc30d88ee96b1e00ef29
oh <yj_oh@naver.com>
2016-11-02 21:36:18 +0900

    test

--- html/main_page.php
+++ html/main_page.php
@@ -183,8 +183,7 @@ $(function(){
                           <li class="active padLR20"><a href="menubar_email.html" class="">e-mail</a></li>
                           <li class="padLR20"><a href="notice.html">공지사항</a></li>
                           <li class="padLR20"><a href="#">스케줄 관리</a></li>
-                          <li class=" dropdown padLR20"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">회의실<span class="caret"></span></a>
-                          <li class="padLR20"><a href="metting.html">팀회의</a></li>
+                          <li class="padLR20"><a href="meeting.html" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">회의실<span class="caret"></span></a>
                           <li class=" dropdown padLR20"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">결재문서 <span class="caret"></span></a>
                               <ul class="dropdown-menu">
                                   <li><a href="document_form.html">품의/제안서</a></li>
@@ -367,7 +366,7 @@ $(function(){
                       <article id="article1">
                       <div class="inbox-head">
                           <h3>Notice</h3>
-                          <span><a href="">more</a></span>
+                          <span><a href="notice/notice.html">more</a></span>
                       </div>
                       <div class="inbox-body" style="padding:0;">
                           <table class="table table-inbox table-hover">
