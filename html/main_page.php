792611ed18f2596ca5f58249ec3aa7bea42e6c5c
DongwooSuh <kehops@kaist.ac.kr>
2016-11-11 20:23:32 +0900

    모듈화함

--- html/main_page.php
+++ html/main_page.php
@@ -20,6 +20,9 @@ header('Content-type: text/html; charset=utf-8');
   <title>Korea Security Company</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <!-- html 모듈들 -->
+  <link rel="import" id="navigation_import" href="navigation.php" />
+
   <!-- CSS 파일들 -->
   <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="css/main_page.css" />
@@ -32,6 +35,22 @@ header('Content-type: text/html; charset=utf-8');
   <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.0.min.js" ></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 
+  <script type='text/javascript'>
+  // on document ready - insert cafe
+  $(function () {
+    $('#nav_bar').load("navigation.php #navigation_container");
+    $('#left_side').load("left_side.php #aside1");
+    $('#aside2').load("chat/chatform.php #chat_side");
+    $('#article5').load("weather.html #weather_section");
+
+    /*
+    $('#cafe_enter').click(function (e) {
+      $('#section1').load("cafe/cafe_main(폼완료).html #cafe_wrapper");
+    });
+    */
+  });
+  </script>
+
   <!-- 채팅창 -->
   <script type='text/javascript' src='chat/chat.js'></script>
   <!-- 날씨 -->
@@ -41,6 +60,7 @@ header('Content-type: text/html; charset=utf-8');
   <script type='text/javascript' src='http://arshaw.com/js/fullcalendar-1.6.3/jquery/jquery-ui-1.10.3.custom.min.js'></script>
   <script type='text/javascript' src='http://arshaw.com/js/fullcalendar-1.6.3/fullcalendar/fullcalendar.min.js'></script>
   <script type='text/javascript' src='js/calendar.js'></script>
+
 </head>
 
 <body>
@@ -48,188 +68,12 @@ header('Content-type: text/html; charset=utf-8');
 <header>
   <div><a href="main_page.php"><img src="images/logo.png" alt="logo" /></a></div>
 </header>
-<!-- //header -->
 
-<!-- 상단 nav -->
-<nav class="navbar navbar-fixed-top">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      </button>
-    </div>
-    <div id="navbar" class="navbar-collapse collapse">
-      <ul class="nav navbar-nav">
-        <li class="active padLR20"><a href="menubar_email.php" class="">e-mail</a></li>
-        <li class="padLR20"><a href="notice/notice.php">공지사항</a></li>
-        <li class="padLR20"><a href="schedule.php">스케줄 관리</a></li>
-        <li class="padLR20"><a href="meeting.php" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">회의실<span class="caret"></span></a>
-        <li class=" dropdown padLR20"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">결재문서 <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="document/document_form.php">품의/제안서</a></li>
-            <li><a href="document/document_board.php">결재진행/완료건</a></li>
-          </ul>
-        </li>
-        <li class=" down padLR20"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">게시판<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="aboard/index.php">건의사항</a></li>
-            <li><a href="board/index.php">자유게시판</a></li>
-          </ul>
-        </li>
-        <li class="padLR20"><a href="cafe/cafe_main.html">cafe</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<!-- //상단 nav -->
+<!-- navigation bar -->
+<nav id="nav_bar" class="navbar navbar-fixed-top"></nav>
 
-<div id="mainbody">
-
-<!-- 좌측 snb -->
-  <div class="container" id="aside1">
-  <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'>
-    <div class="mail-box">
-      <aside class="sm-side">
-          <div class="user-head">
-            <?php
-              $query = "SELECT * FROM user WHERE prodID=" . $prodID;
-              $result = $db->query($query);
-              $row = $result->fetch_assoc();
-              $user_name = $row['name'];
-              $user_mail = $row['mail'];
-              $user_position = $row['position'];
-              $image_path = $row['image'];
-             ?>
-              <a class="inbox-avatar" href="javascript:;">
-              <img width="64" height="60" src=
-            <?php
-              echo "\"images/userImages/" . $image_path . "\""
-             ?>
-              alt="사용자사진" />
-              </a>
-              <!-- 로그인된 사원 정보 -->
-              <div class="user-name">
-                <h5>
-                  <?php
-                  echo $user_name;
-                  ?>
-                </h5>
-                <!-- 직책 -->
-                <span>
-                  <?php
-                  echo $user_position
-                   ?>
-                </span>
-                <!-- 이메일 -->
-                <span>
-                  <?php
-                  echo $user_mail
-                   ?>
-                </span>
-              </div>
-          </div>
-          <div class="inbox-body">
-              <a href="/login/logout.php"class="btn btn-compose">Logout</a>
-              <!-- Modal -->
-              <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade" style="display: none;">
-                  <div class="modal-dialog">
-                      <div class="modal-content">
-                          <div class="modal-header">
-                              <button aria-hidden="true" data-dismiss="modal" class="close" type="button">×</button>
-                              <h4 class="modal-title">Compose</h4>
-                          </div>
-                          <div class="modal-body">
-                              <form role="form" class="form-horizontal">
-                                  <div class="form-group">
-                                      <label class="col-lg-2 control-label">To</label>
-                                      <div class="col-lg-10">
-                                          <input type="text" placeholder="" id="inputEmail1" class="form-control">
-                                      </div>
-                                  </div>
-                                  <div class="form-group">
-                                      <label class="col-lg-2 control-label">Cc / Bcc</label>
-                                      <div class="col-lg-10">
-                                          <input type="text" placeholder="" id="cc" class="form-control">
-                                      </div>
-                                  </div>
-                                  <div class="form-group">
-                                      <label class="col-lg-2 control-label">Subject</label>
-                                      <div class="col-lg-10">
-                                          <input type="text" placeholder="" id="inputPassword1" class="form-control">
-                                      </div>
-                                  </div>
-                                  <div class="form-group">
-                                      <label class="col-lg-2 control-label">Message</label>
-                                      <div class="col-lg-10">
-                                          <textarea rows="10" cols="30" class="form-control" id="" name=""></textarea>
-                                      </div>
-                                  </div>
-
-                                  <div class="form-group">
-                                      <div class="col-lg-offset-2 col-lg-10">
-                                          <span class="btn green fileinput-button">
-                                            <i class="fa fa-plus fa fa-white"></i>
-                                            <span>Attachment</span>
-                                            <input type="file" name="files[]" multiple="">
-                                          </span>
-                                          <button class="btn btn-send" type="submit">Send</button>
-                                      </div>
-                                  </div>
-                              </form>
-                          </div>
-                      </div><!-- /.modal-content -->
-                  </div><!-- /.modal-dialog -->
-              </div><!-- /.modal -->
-          </div>
-          <ul class="inbox-nav inbox-divider">
-              <li class="active">
-                  <a href="#"><i class="fa fa-inbox"></i> 수신메일함 <span class="label label-danger pull-right">2</span></a>
-              </li>
-              <li class="active">
-                  <a href="#"><i class="fa fa-inbox"></i> 발신메일함 </a>
-              </li>
-              <li>
-                  <a href="#"><i class="fa fa-envelope-o"></i> 메일보내기</a>
-              </li>
-              <li>
-                  <a href="#"><i class=" fa fa-trash-o"></i> 지운메일함</a>
-              </li>
-          </ul>
-
-          <ul class="nav nav-pills nav-stacked labels-info ">
-              <li> <h4>Buddy online</h4> </li>
-              <li> <a href="#"> <i class=" fa fa-circle text-success"></i>Alireza Zare <p>I do not think</p></a>  </li>
-              <li> <a href="#"> <i class=" fa fa-circle text-danger"></i>Dark Coders<p>Busy with coding</p></a> </li>
-              <li> <a href="#"> <i class=" fa fa-circle text-muted "></i>Mentaalist <p>I out of control</p></a>
-              </li><li> <a href="#"> <i class=" fa fa-circle text-muted "></i>H3s4m<p>I am not here</p></a>
-              </li><li> <a href="#"> <i class=" fa fa-circle text-muted "></i>Dead man<p>I do not think</p></a>
-              </li>
-          </ul>
-
-          <div class="inbox-body text-center">
-              <div class="btn-group">
-                  <a class="btn mini btn-primary" href="javascript:;">
-                      <i class="fa fa-plus"></i>
-                  </a>
-              </div>
-              <div class="btn-group">
-                  <a class="btn mini btn-success" href="javascript:;">
-                      <i class="fa fa-phone"></i>
-                  </a>
-              </div>
-              <div class="btn-group">
-                  <a class="btn mini btn-info" href="javascript:;">
-                      <i class="fa fa-cog"></i>
-                  </a>
-              </div>
-          </div>
-    </aside>
-  </div>
-  </div>
-<!-- //좌측 snb -->
+<!-- left side bar -->
+<div id="left_side"></div>
 
 <!-- section -->
 <section id="section1">
@@ -398,87 +242,15 @@ header('Content-type: text/html; charset=utf-8');
       </div>
     </article>
 
-    <article id="article5" style="background: #fff;">
-      <div class="weather_header">
-        현재 날씨
-      </div>
-      <div class="weather_box">
-        <span id="weather_main">
-          <!-- 기상청 정보로 채워짐 -->
-        </span>
-      </div>
-      <div class="weather_box">
-        <span>기온</span>
-        <span id="weather_temp">
-          <!-- 기상청 정보로 채워짐 -->
-        </span>
-        <span>도</span>
-      </div>
-      <div class="weather_box">
-        <span>습도</span>
-        <span id="weather_humidity">
-          <!-- 기상청 정보로 채워짐 -->
-        </span>
-        <span>%</span>
-      </div>
-    </article>
+    <!-- 날씨 -->
+    <article id="article5"></arctile>
+
   </section>
 </section>
 <!-- //위젯 -->
 
 <!-- 우측 chat -->
-  <aside id="aside2">
-    <!-- chat s_c  -->
-    <div class="chat_window">
-      <div class="top_menu">
-        <div class="goback">목록보기</div>
-        <div class="chat_title">채팅목록</div>
-        <!-- 채팅 제목 -->
-        <div id="chatroom_num"></div>
-      </div>
-      <div class="chat_main" id="chat_main">
-        <!-- 채팅방 목록이 진열됨 -->
-        <div id="chat_list" class="chatroom_list">
-          <!-- 채팅방으로 유저 초대 후 채팅방 생성 -->
-          <input type="text" class="input_receiver" name="receiver" placeholder="invite user">
-          <button class="create_chat_btn" id="make_chat_btn" name="make_chat_btn">
-            채팅방 만들기
-          </button>
-          <div id="chatlist_list">
-          </div>
-          <!-- 채팅방 목록 템플릿 - 표시되지 않음 -->
-          <div class="chatlist_template">
-            <div class="chatlist_elem">
-              <span id="chatlist_number"></span>
-              <span id="chatlist_lastActive"></span>
-            </div>
-          </div>
-        </div>
-        <!-- 대화 내용이 표시됨 -->
-        <ul class="messages"></ul>
-      </div>
-      <!-- 채팅 입력창 및 보내기 버튼 -->
-      <div class="bottom_wrapper clearfix">
-        <div class="message_input_wrapper">
-          <input id="message_input" class="message_input" placeholder="Type your message here..." />
-        </div>
-        <div id="send_message_btn" class="send_message">
-          <div class="icon"></div>
-          <div class="text">Send</div>
-        </div>
-      </div>
-    </div>
-    <!-- 메세지 템플릿 - 숨겨져있음 -->
-    <div class="message_template">
-      <li class="message">
-        <div class="avatar"></div>
-        <div class="text_wrapper">
-          <div class="text"></div>
-        </div>
-      </li>
-    </div>
-</aside>
-<!-- 우측 채팅창 끝 -->
+<aside id="aside2"></aside>
 
 </body>
 </html>
---
+++ html/navigation.php
@@ -0,0 +1,35 @@
+<!-- 상단 nav -->
+<nav id="navigation_bar" class="navbar navbar-fixed-top">
+  <div id="navigation_container" class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li class="active padLR20"><a href="menubar_email.php" class="">e-mail</a></li>
+        <li class="padLR20"><a href="notice/notice.php">공지사항</a></li>
+        <li class="padLR20"><a href="schedule.php">스케줄 관리</a></li>
+        <li class="padLR20"><a href="meeting.php" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">회의실<span class="caret"></span></a>
+        <li class=" dropdown padLR20"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">결재문서 <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="document/document_form.php">품의/제안서</a></li>
+            <li><a href="document/document_board.php">결재진행/완료건</a></li>
+          </ul>
+        </li>
+        <li class=" down padLR20"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">게시판<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="aboard/index.php">건의사항</a></li>
+            <li><a href="board/index.php">자유게시판</a></li>
+          </ul>
+        </li>
+        <li class="padLR20" id='cafe_enter'>cafe</li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- //상단 nav -->
